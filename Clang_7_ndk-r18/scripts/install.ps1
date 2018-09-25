9a5770e34ffec8ba4782e45fa6371f745818de7b
Benn Herrera <bherrera@reification.io>
2018-09-25 11:08:01 -0700

    initial commit

---
+++ Clang_7_ndk-r18/scripts/install.ps1
@@ -0,0 +1,107 @@
+$id=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal=new-object System.Security.Principal.WindowsPrincipal($id)
+$role=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+if (!$principal.IsInRole($role)) {
+   $pinfo = New-Object System.Diagnostics.ProcessStartInfo "powershell"
+   [string[]]$arguments = @("-ExecutionPolicy","Bypass",$myInvocation.MyCommand.Definition)
+   foreach ($key in $myInvocation.BoundParameters.Keys) {
+       $arguments += ,@("-$key")
+       $arguments += ,@($myInvocation.BoundParameters[$key])
+    }
+   $pinfo.Arguments = $arguments
+   $pinfo.Verb = "runas"
+   $null = [System.Diagnostics.Process]::Start($pinfo)
+   exit
+}
+
+$Host.UI.RawUI.WindowTitle = "Reification Android NDK r18 Clang 7 MSVS Toolset Installer"
+
+function Install-Failed($message) {
+    Write-Error $message
+    Write-Host -NoNewLine "Press any key to continue..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    exit
+}
+
+function Get-Registry($key, $valuekey = "") {
+    $reg = Get-Item -Path $key -ErrorAction SilentlyContinue
+    if ($reg) {
+        return $reg.GetValue($valuekey)
+    }
+    return $null
+}
+
+$VSInstallDir = Get-Registry Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\VisualStudio\SxS\VS7 "15.0"
+if (!$VSInstallDir) {
+    $VSInstallDir = Get-Registry Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7 "15.0"
+}
+
+$VSArm64ToolsetsDir = "${VSInstallDir}Common7\IDE\VC\VCTargets\Application Type\Android\3.0\Platforms\ARM64\PlatformToolsets"
+$ClangToolsetName = "Clang_7_ndk-r18"
+$TargetToolsetPath = "$VSArm64ToolsetsDir\$ClangToolsetName"
+
+if (!$VSInstallDir) {
+    Install-Failed "Visual Studio 2017 not installed."
+}
+
+if(!(Test-Path $VSArm64ToolsetsDir)) {
+    Install-Failed( "Visual Studio C++ Mobile Workflow for Android not installed." )
+}
+
+$rootDir = Split-Path -Parent $myInvocation.MyCommand.Definition | Split-Path -Parent
+$SourceToolsetPath = "$rootDir\assets\Clang_7"
+
+function Install () {
+    if(Test-Path "$TargetToolsetPath") {
+        Remove-Item "$TargetToolsetPath" -Recurse
+    }
+    Copy-Item -Path "$SourceToolsetPath" -Destination "$TargetToolsetPath" -Recurse -Force
+
+    "Toolset integration installed."
+}
+
+function Uninstall() {
+    if(Test-Path "$TargetToolsetPath") {
+        Remove-Item "$TargetToolsetPath" -Recurse
+    }
+
+    "Toolset integration uninstalled."
+}
+
+
+""
+"=== Install configuration ==="
+"* Target Platform: Android ARM64"
+"* Clang 7.0 toolset: $ClangToolsetName"
+"* At $TargetToolsetPath"
+""
+
+$Operation = "Install"
+
+if(Test-Path $TargetToolsetPath) {
+    if ((Read-Host "Uninstall? (N/y)") -match "y|yes") {
+        $Operation = "Uninstall"
+    } else {
+        $Operation = "Re-Install"
+    }
+}
+
+if( $Operation -ne "Uninstall" ) {
+    if ((Read-Host "${Operation}? (Y/n)") -match "n|no") {
+        "operation canceled by user."
+        Write-Host -NoNewLine "Press any key to continue..."
+        $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+        exit
+    }
+}
+
+if ($Operation -eq "Uninstall") {
+    Uninstall
+} else {
+    Install
+}
+
+Write-Host -NoNewLine "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+""
---
+++ README.txt
@@ -0,0 +1,3 @@
+These MSVS toolsets are integrations for MSVS android native build workflow for NDK versions not yet supported by Microsoft
+They likely will not work for uses beyond what Reification is putting them to. If we want to share them they will need some finishing up work.
+
